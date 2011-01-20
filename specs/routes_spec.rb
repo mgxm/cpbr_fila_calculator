ec059e019d90029a8d421f34a6e5aac45b65ce45
marciogm <marciogiaxa@gmail.com>
2011-01-20 04:48:59 -0200

    fixed route test

--- specs/routes_spec.rb
+++ specs/routes_spec.rb
@@ -1,14 +1,13 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
-describe 'boot' do
+describe "Routes" do
   include Rack::Test::Methods
-
-  describe "test routes" do
-
-    it 'should respond to /' do
-      get '/'
-      last_response.should be_ok
-    end
-
+  def app
+    @app ||= Sinatra::Application
+  end
+  
+  it 'should respond to /' do
+    get '/'
+    last_response.should be_ok
   end
 end
