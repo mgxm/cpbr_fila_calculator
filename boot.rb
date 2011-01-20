b620d2bd47ec674a2a0b0d4375de44e94f5487b9
Caio Tarifa <caio@formaweb.com.br>
2011-01-20 02:42:03 -0200

    trying to include sass

--- boot.rb
+++ boot.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'haml'
+require 'sass'
 
-set :views, File.dirname(__FILE__) + "/app/views/"
+set :views, File.dirname(__FILE__) + '/app/views'
+set :public, File.dirname(__FILE__) + '/public'
 
 get '/' do
   haml :index
+end
+
+get '/stylesheets/application.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :'/stylesheets/application'
 end
