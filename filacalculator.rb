a543583ba0335360c0dfa879d33d9b0617d5e1c2
marciogm <marciogiaxa@gmail.com>
2011-01-20 20:40:22 -0200

    show queue on index

--- filacalculator.rb
+++ filacalculator.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'haml'
 require 'sass'
+require File.dirname(__FILE__) + '/app/models/calculator'
 
 set :views, File.dirname(__FILE__) + '/app/views'
 set :public, File.dirname(__FILE__) + '/public'
 
 get '/' do
+  @calculator = Calculator.new
+  @banheiro = @calculator.select('banheiro')
   haml :index
 end
 
